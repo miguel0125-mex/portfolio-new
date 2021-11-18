b7a8a9ba3a8f2f46781d884e82e45c436e9b6d04
Miguel Ruiz <later481@gmail.com>
2021-11-17 22:01:21 -0600

    cambios javascript 2

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const d = document;
 
 d.addEventListener('DOMContentLoaded', () => {
     showMenu("toggleId", "menuId");
+    link.forEach(n => n.addEventListener('click', linkAction));
 });
-const showMenu = (toggleId, menuId) => {
+const showMenu = (toggleId, menuId, link_menu) => {
     const toggle = d.getElementById(toggleId),
-        nav = d.getElementById(menuId)
+        nav = d.getElementById(menuId);
 
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
@@ -13,4 +14,15 @@ const showMenu = (toggleId, menuId) => {
             nav.classList.toggle('is-active');
         })
     }
+}
+const link = d.querySelectorAll("#link");
+const menu = d.getElementById('menuId');
+const btnMenu = d.getElementById('toggleId');
+
+function linkAction() {
+    link.forEach(n => n.classList.remove());
+    this.classList.add();
+
+    menu.classList.remove("is-active");
+    btnMenu.classList.remove('is-active');
 }
