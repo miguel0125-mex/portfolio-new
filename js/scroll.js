acad7efa4a6d9365507e07b5e87bca9be918d8ce
Miguel Ruiz <later481@gmail.com>
2021-12-13 18:14:43 -0600

    icono agregado

---
+++ js/scroll.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal(".inicio", { interval: 700, origin: "bottom", reset: true, delay: 1000 });
+ScrollReveal().reveal(".about_info", { duration: 700, origin: "bottom", reset: true });
+ScrollReveal().reveal(".about_photo", { duration: 700, origin: "bottom", reset: true, delay: 1000 });
+ScrollReveal().reveal(".works", { duration: 700, origin: "bottom", reset: true, delay: 1000 });
+ScrollReveal().reveal(".contact", { duration: 700, origin: "bottom", reset: true, delay: 1000 });
